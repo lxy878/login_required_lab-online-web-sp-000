d2f3a23099c9caa03b7efb924f69bcf9fd204b01
Xiaoyi Liu <lxy878@gmail.com>
2020-07-10 17:13:50 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,13 @@
+class SecretsController < ApplicationController
+  before_action :require_login, only: [:show]
+  def index
+  end
+
+  def show
+  end
+
+  private
+    def require_login
+      redirect_to sessions_new_path if !session.include? :name
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  post 'sessions/create'
+
+  post 'sessions/destroy'
+
+  get 'secrets/index'
+
+  get 'secrets/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
