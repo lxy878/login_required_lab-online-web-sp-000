e384d2145cd2a441374acac0b2d6c61c5939c719
Xiaoyi Liu <lxy878@gmail.com>
2020-07-10 16:58:53 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  
+
   post 'sessions/create'
 
   post 'sessions/destroy'
 
+  get 'secrets/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
